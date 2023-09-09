37dce337202d90041a7a925aa16ec687133d5784
philipmo <philipmo59@gmail.com>
2023-09-08 23:37:57 -0400

    finished

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_list = [0,1]
+    for num in range(length - 2):
+        answer = fibonacci_list[-1] + fibonacci_list[-2]
+        fibonacci_list.append(answer)
+    print(fibonacci_list)
+
+print_fibonacci(9)
